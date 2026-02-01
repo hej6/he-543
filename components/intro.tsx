6578c56e05023582c7c1db7b64f15c108ab8cccc
hej6 <he.jared0@gmail.com>
2026-02-01 18:55:42 -0500

    Implemented functionality from assignment 02. Includes header, paragraph, unordered list and list items.

---
+++ components/intro.tsx
@@ -0,0 +1,17 @@
+export default function Intro() {
+	return (
+		<div>
+			<h1>The CIA Triad</h1>
+			<br></br>
+
+			<p>The CIA Triad is a fundamental model for network security. It stands for the following:</p>
+			<br></br>
+
+			<ul>
+				<li>Confidentiality</li>
+				<li>Integrity</li>
+				<li>Availability</li>
+			</ul>
+		</div>
+	);
+}
